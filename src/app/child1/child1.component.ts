cfad754c1941cd4bc7cc94666433670e7babddde
anton.zyablitsev <anton.zyablitsev@m.thelightech.com>
2023-03-17 12:03:10 +0300

    Refresh-parent

--- src/app/child1/child1.component.ts
+++ src/app/child1/child1.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { AppService } from "../app.service";
 
 @Component({
   selector: 'app-child1',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./child1.component.scss']
 })
 export class Child1Component {
+  name = 'Child 1';
+  count = 0;
+
+  constructor(
+    @Inject(AppService) private readonly _service: AppService
+  ) {}
+
+  numberClick(): void {
+    this.count = this.count + 1;
+    this._service.countClick(this.name, this.count);
+  }
 
 }
